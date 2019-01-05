37b0436ae59e0f77d479350132495fbb70ac5c3c
Rusty Mina <rustymina@gmail.com>
2019-01-06 00:04:08 +0800

    Added batch style transfer for folders

--- stylize.py
+++ stylize.py
@@ -2,6 +2,7 @@ import torch
 import utils
 import transformer
 import os
+from torchvison import datasets, transforms
 
 STYLE_TRANSFORM_PATH = "transforms/mosaic_dark2.pth"
 #CONTENT_IMAGE_PATH = "images/rusty.jpg"
@@ -52,5 +53,59 @@ def stylize_folder_single(style_path, content_folder, save_folder):
 
             # Save image
             utils.saveimg(generated_image, save_folder + image_name)
+
+def stylize_folder_batch(style_path, content_folder, save_folder, batch_size=1):
+    """Stylizes images in a folder by batch
+    If the images  are of different dimensions, use transform.resize() or use a batch size of 1
+    """
+    # Device
+    device = ("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Image loader
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    image_dataset = ImageFolderWithPaths(content_folder, transform=transform)
+    image_loader = torch.utils.data.DataLoader(image_dataset, batch_size=batch_size)
+
+    # Load Transformer Network
+    net = transformer.TransformerNetwork()
+    net.load_state_dict(torch.load(style_path))
+    net = net.to(device)
+
+    # Stylize batches of images
+    with torch.no_grad():
+        for content_batch, _, path in image_loader:
+            # Free-up unneeded cuda memory
+            torch.cuda.empty_cache()
+
+            # Generate image
+            generated_tensor = net(content_batch).detach()
+
+            # Save images
+            count=0
+            for i in range(batch_size):
+                generated_image = utils.ttoi(generated_tensor[i])
+                image_name = os.path.basename(*path)
+                utils.saveimg(generated_image, save_folder + image_name)
+                count+=1
+
+class ImageFolderWithPaths(datasets.ImageFolder):
+    """Custom dataset that includes image file paths. 
+    Extends torchvision.datasets.ImageFolder()
+    Reference: https://discuss.pytorch.org/t/dataloader-filenames-in-each-batch/4212/2
+    """
+    # override the __getitem__ method. this is the method dataloader calls
+    def __getitem__(self, index):
+        # this is what ImageFolder normally returns 
+        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+
+        # the image file path
+        path = self.imgs[index][0]
+
+        # make a new tuple that includes original and the path
+        tuple_with_path = (*original_tuple, path)
+        return tuple_with_path
 
 #stylize()
