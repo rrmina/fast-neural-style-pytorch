ccc5dd0210b0811eb69b657294c40b8751ee24f0
Rusty Mina <rustymina@gmail.com>
2019-03-25 22:07:44 +0800

    Added more computationally efficient models

--- transformer.py
+++ transformer.py
@@ -61,6 +61,82 @@ class TransformerNetworkTanh(TransformerNetwork):
     def forward(self, x):
         return super(TransformerNetworkTanh, self).forward(x) * self.tanh_multiplier
 
+class TransformerNetworkV2(nn.Module):
+    """
+    Feedforward Transformation NetworkV2
+    
+        - No Tanh
+        + Using Fully Pre-activated Residual Layers 
+    """
+    def __init__(self):
+        super(TransformerNetworkV2, self).__init__()
+        self.ConvBlock = nn.Sequential(
+            ConvLayer(3, 32, 9, 1),
+            nn.ReLU(),
+            ConvLayer(32, 64, 3, 2),
+            nn.ReLU(),
+            ConvLayer(64, 128, 3, 2),
+            nn.ReLU()
+        )
+        self.ResidualBlock = nn.Sequential(
+            ResidualLayerV2(128, 3),
+            ResidualLayerV2(128, 3),
+            ResidualLayerV2(128, 3),
+            ResidualLayerV2(128, 3),
+            ResidualLayerV2(128, 3)
+        )
+        self.DeconvBlock = nn.Sequential(
+            DeconvLayer(128, 64, 3, 2, 1),
+            nn.ReLU(),
+            DeconvLayer(64, 32, 3, 2, 1),
+            nn.ReLU(),
+            ConvLayer(32, 3, 9, 1, norm="None")
+        )
+
+    def forward(self, x):
+        x = self.ConvBlock(x)
+        x = self.ResidualBlock(x)
+        out = self.DeconvBlock(x)
+        return out
+
+class TransformerResNextNetwork(nn.Module):
+    """
+    Feedforward Transformation Network - ResNeXt
+    
+        - No Tanh
+        + ResNeXt Layer
+    """
+    def __init__(self):
+        super(TransformerResNextNetwork, self).__init__()
+        self.ConvBlock = nn.Sequential(
+            ConvLayer(3, 32, 9, 1),
+            nn.ReLU(),
+            ConvLayer(32, 64, 3, 2),
+            nn.ReLU(),
+            ConvLayer(64, 128, 3, 2),
+            nn.ReLU()
+        )
+        self.ResidualBlock = nn.Sequential(
+            ResNextLayer(128, [64, 64, 128], kernel_size=3),
+            ResNextLayer(128, [64, 64, 128], kernel_size=3),
+            ResNextLayer(128, [64, 64, 128], kernel_size=3),
+            ResNextLayer(128, [64, 64, 128], kernel_size=3),
+            ResNextLayer(128, [64, 64, 128], kernel_size=3)
+        )
+        self.DeconvBlock = nn.Sequential(
+            DeconvLayer(128, 64, 3, 2, 1),
+            nn.ReLU(),
+            DeconvLayer(64, 32, 3, 2, 1),
+            nn.ReLU(),
+            ConvLayer(32, 3, 9, 1, norm="None")
+        )
+
+    def forward(self, x):
+        x = self.ConvBlock(x)
+        x = self.ResidualBlock(x)
+        out = self.DeconvBlock(x)
+        return out
+
 class ConvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, norm="instance"):
         super(ConvLayer, self).__init__()
@@ -88,6 +164,11 @@ class ConvLayer(nn.Module):
         return out
 
 class ResidualLayer(nn.Module):
+    """
+    Deep Residual Learning for Image Recognition
+
+    https://arxiv.org/abs/1512.03385
+    """
     def __init__(self, channels=128, kernel_size=3):
         super(ResidualLayer, self).__init__()
         self.conv1 = ConvLayer(channels, channels, kernel_size, stride=1)
@@ -100,6 +181,78 @@ class ResidualLayer(nn.Module):
         out = self.conv2(out)            # 2nd conv layer
         out = out + identity             # add residual
         return out
+
+class ResidualLayerV2(nn.Module):
+    """
+    Identity Mappings in Deep Residual Networks
+        
+        Full pre-activation
+
+    https://arxiv.org/abs/1603.05027
+    """
+    def __init__(self, channels=128, kernel_size=3):
+        super(ResidualLayerV2, self).__init__()
+        self.conv1 = NormReluConv(channels, channels, kernel_size, stride=1)
+        self.conv2 = NormReluConv(channels, channels, kernel_size, stride=1)
+
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = out + identity
+        return out
+
+class ResNextLayer(nn.Module):
+    """
+    Aggregated Residual Transformations for Deep Neural Networks
+
+        Equal to better performance with 10x less parameters
+
+    https://arxiv.org/abs/1611.05431
+    """
+    def __init__(self, in_ch=128, channels=[64, 64, 128], kernel_size=3):
+        super(ResNextLayer, self).__init__()
+        ch1, ch2, ch3 = channels
+        self.conv1 = ConvLayer(in_ch, ch1, kernel_size=1, stride=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = ConvLayer(ch1, ch2, kernel_size=kernel_size, stride=1)
+        self.relu2 = nn.ReLU()
+        self.conv3 = ConvLayer(ch2, ch3, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        identity = x
+        out = self.relu1(self.conv1(x))
+        out = self.relu2(self.conv2(out))
+        out = self.conv3(out)
+        out = out + identity
+        return out
+
+class NormReluConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, norm="instance"):
+        super(NormConv, self).__init__()
+
+        # Normalization Layers
+        if (norm=="instance"):
+            self.norm_layer = nn.InstanceNorm2d(in_channels, affine=True)
+        elif (norm=="batch"):
+            self.norm_layer = nn.BatchNorm2d(in_channels, affine=True)
+
+        # ReLU Layer
+        self.relu_layer = nn.ReLU()
+
+        # Padding Layers
+        padding_size = kernel_size // 2
+        self.reflection_pad = nn.ReflectionPad2d(padding_size)
+
+        # Convolution Layer
+        self.conv_layer = nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        x = self.norm_layer(x)
+        x = self.relu_layer(x)
+        x = self.reflection_pad(x)
+        x = self.conv_layer(x)
+        return x
 
 class DeconvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, output_padding, norm="instance"):
